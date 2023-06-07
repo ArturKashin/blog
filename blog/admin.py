29d0dae7b9fc0ae7e5a86fe89282d89d91b80861
Artur <ebay.kashin@yandex.ru>
2023-06-07 09:45:28 +0300

    blog

---
+++ blog/admin.py
@@ -0,0 +1,53 @@
+from django.contrib import admin
+from .models import *
+from django.utils.safestring import mark_safe
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
+
+
+class BlogAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Blog
+        fields = '__all__'
+
+
+class BlogAdmin(admin.ModelAdmin):
+    form = BlogAdminForm
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('id', 'title', 'time_created', 'get_html_photo', 'is_published')
+    list_display_links = ('id', 'title')
+    # регистр-независимый поиск
+    search_fields = ('title__iregex', 'content__iregex')
+    list_editable = ('is_published',)
+    list_filter = ('is_published', 'time_created')
+    # продублировать панель сверху в админке
+    save_on_top = True
+    # порядок отображения в админке
+    fields = ('title', 'slug', 'cat', 'content', 'photo', 'get_html_photo', 'is_published', 'time_created',
+              'time_update')
+    readonly_fields = ('get_html_photo', 'time_created', 'time_update')
+
+    # отображение картинок в админке
+    def get_html_photo(self, object):
+        if object.photo:
+            return mark_safe(f"<img src='{object.photo.url}' width='50'>")
+
+    get_html_photo.short_description = 'Миниатюра'
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name__iregex',)
+
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+
+admin.site.site_title = 'Админ-панель блога'
+admin.site.site_header = 'Админ-панель блога'
